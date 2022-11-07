394a9d0b4695cd10380547f2d2574d4ce6c458af
prathamesh2525 <prathameshmadniwale31@gmail.com>
2022-11-08 01:47:27 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
+})
+
+//Socket
+
+io.on("connection", (socket) => {
+  console.log("Connected...")
+
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg)
+  })
+})
